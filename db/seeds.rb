a28930641c2ffb6080159ca66e9d760cdf6450d9
Steven Broderick <brodericksteven@gmail.com>
2016-01-08 15:58:06 -0800

    Finish Abios queryhandler class

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,12 @@
-require "pp"
+
 
 class Abios
   def initialize
-    @api_key          = ENV["ABIOS_API_KEY"]
-    @games_root       = "https://api.abiosgaming.com/v1/games?access_token="
-    @matches_root     = "https://api.abiosgaming.com/v1/matches?access_token="
-    @competitors_root = "https://api.abiosgaming.com/v1/competitors?access_token="
-    @tournaments_root = "https://api.abiosgaming.com/v1/tournaments?access_token="
-    @streams_root     = "" #TBD
+    @api_key          = "?access_token=#{ENV['ABIOS_API_KEY']}"
+    @games_root       = "https://api.abiosgaming.com/v1/games"
+    @matches_root     = "https://api.abiosgaming.com/v1/matches"
+    @competitors_root = "https://api.abiosgaming.com/v1/competitors"
+    @tournaments_root = "https://api.abiosgaming.com/v1/tournaments"
   end
 
   def get_games
@@ -19,17 +18,18 @@ class Abios
   end
 
   def get_competitors
+    # TODO
   end
 
   def get_competitors_by_tournament(tournament_id)
-    filter = "&tournament[]=" + tournament_id.to_s
+    filter = "&tournaments[]=" + tournament_id.to_s
     HTTParty.get(@competitors_root + @api_key + filter)
   end
 
-  def get_streams_by_tournament(tournament_id)
+  def get_stream_by_tournament(tournament_id)
+    HTTParty.get(@tournaments_root + "/" + tournament_id.to_s + @api_key).fetch("url")
   end
 
-  # not working yet
   def get_tournaments_by_game(game_id)
     filter = "&games[]=" + game_id.to_s
     HTTParty.get(@tournaments_root + @api_key + filter)
@@ -38,12 +38,11 @@ class Abios
   private
 
   def get_matches
+    HTTParty.get(@matches + @api_key)
   end
 
   def get_matches_by_tournament(tournament_id)
-    filter = "&tournament[]=" + tournament_id.to_s
+    filter = "&tournaments[]=" + tournament_id.to_s
     HTTParty.get(@matches_root + @api_key + filter)
   end
 end
-
-binding.pry
