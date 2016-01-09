fec0db7396861bb7f6ed930de9f740a0e1deca20
Steven Broderick <brodericksteven@gmail.com>
2016-01-08 16:57:11 -0800

    Create game-seeding block (only seeds games currently)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-
-
 class Abios
   def initialize
     @api_key          = "?access_token=#{ENV['ABIOS_API_KEY']}"
@@ -10,11 +8,16 @@ class Abios
   end
 
   def get_games
+    puts "getting all games"
     HTTParty.get(@games_root + @api_key)
   end
 
   def get_tournaments
+    puts "getting all tournaments"
     HTTParty.get(@tournaments_root + @api_key)
+  end
+
+  def get_matches_by_tournament
   end
 
   def get_competitors
@@ -22,15 +25,18 @@ class Abios
   end
 
   def get_competitors_by_tournament(tournament_id)
+    puts "tournament_id: #{tournament_id}"
     filter = "&tournaments[]=" + tournament_id.to_s
     HTTParty.get(@competitors_root + @api_key + filter)
   end
 
   def get_stream_by_tournament(tournament_id)
+    puts "tournament_id: #{tournament_id}"
     HTTParty.get(@tournaments_root + "/" + tournament_id.to_s + @api_key).fetch("url")
   end
 
   def get_tournaments_by_game(game_id)
+    puts "game_id: #{game_id}"
     filter = "&games[]=" + game_id.to_s
     HTTParty.get(@tournaments_root + @api_key + filter)
   end
@@ -38,6 +44,7 @@ class Abios
   private
 
   def get_matches
+    "getting matches"
     HTTParty.get(@matches + @api_key)
   end
 
@@ -46,3 +53,42 @@ class Abios
     HTTParty.get(@matches_root + @api_key + filter)
   end
 end
+
+a = Abios.new
+
+GAMES       = a.get_games
+TOURNAMENTS = GAMES.map       {|game| a.get_tournaments_by_game(game["id"])}.flatten
+COMPETITORS = TOURNAMENTS.map {|tournament| a.get_competitors_by_tournament(tournament["id"])}
+STREAMS     = TOURNAMENTS.map {|tournament| a.get_stream_by_tournament(tournament["id"])}
+
+GAMES.each do |game|
+
+  Game.create(
+    id:   game["id"],
+    name: game["title"]
+    )
+end
+
+binding-pry
+
+# TOURNAMENTS.each do |tournament|
+#   Tournament.create(
+#     id:         tournament["id"],
+#     name:       tournament["title"],
+#     start_date: tournament["start"],
+#     end_date:   tournament["end"],
+#     game_id:    tournament["game"]["id"]
+#     )
+# end
+
+# COMPETITORS.each do |competitor|
+#   Competitor.create(
+#     id:            competitor["id"],
+#     name:
+#     tournament_id:
+#     )
+# end
+
+# STREAMS.each do |stream|
+
+# end
