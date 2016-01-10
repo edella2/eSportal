50cc663f2e8ef0d4829476276fe99c3c87659c01
Armani Saldana <armanisaldana@gmail.com>
2016-01-09 16:35:07 -0800

    user can log out

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'tournaments#index'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+ 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
