6c7952996fa72d70dc797728a3b2f64a8f5f6ee8
Armani Saldana <armanisaldana@gmail.com>
2016-01-09 22:18:53 -0800

    fix timertests controller

--- app/controllers/timertests_controller.rb
+++ app/controllers/timertests_controller.rb
@@ -1,20 +1,5 @@
 class TimertestsController < ApplicationController
 	def index
-		if params[:search]
-      @tournaments = Tournament.search(params[:search]).order("created_at DESC")
-    else
-      case params[:sort_option]
-      when "year"
-        @tournaments = sort_tournaments_by_year
-      when "month"
-        @tournaments = sort_tournaments_by_month
-      when "week"
-        @tournaments = sort_tournaments_by_week
-      when "day"
-        @tournaments = sort_tournaments_by_day
-      else
-        @tournaments = Tournament.order('created_at DESC')
-      end
-    end
+
 	end
 end
