cbe338016eb3208034c40b068a9bd1e906d2f730
gastongouron <gastongouron@gmail.com>
2016-01-11 10:52:52 -0800

    WIP

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,2 @@
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w( timer.js )
+Rails.application.config.assets.precompile += %w( timer.js toggleinfo.js )
