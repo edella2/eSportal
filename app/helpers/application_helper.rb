9dbdbe74a2af9bbfda8900faa236cc4956c409f7
gastongouron <gastongouron@gmail.com>
2016-01-11 14:18:20 -0800

    Fix js and views details

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+def create_link(text, path)
+    class_name = current_page?(path) ? 'active' : ''
+    content_tag(:li, class: class_name) do
+    link_to text, path
+  end
+end
+
 end
