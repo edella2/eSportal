e07af777bdd0a8eb2f5589a7bc82582174fba607
Eric DellAringa <edella2@gmail.com>
2016-01-11 18:02:24 -0800

    Add console outputs to troubleshoot heroku

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -11,14 +11,36 @@ class SubscriptionsController < ApplicationController
       'start' => { 'dateTime' => Chronic.parse('tomorrow 4 pm') },
       'end' => { 'dateTime' => Chronic.parse('tomorrow 5pm') } }
 
+    p "*" * 50
+    p "@event"
+    p @event
+    p "*" * 50
+
     client = Google::APIClient.new
+    p "*" * 50
+    p "Client"
+    p client
+    p "*" * 50
     client.authorization.access_token = current_user.token
+    p "*" * 50
+    p "client.authorization"
+    p client.authorization.access_token
+    p "*" * 50
     service = client.discovered_api('calendar', 'v3')
+    p "*" * 50
+    p "service"
+    p service
+    p "*" * 50
 
     @set_event = client.execute(api_method: service.events.insert,
                                 parameters: { 'calendarId' => 'primary', 'sendNotifications' => true },
                                 body: JSON.dump(@event),
                                 headers: { 'Content-Type' => 'application/json' })
+
+    p "*" * 50
+    p "@set_event"
+    p @set_event
+    p "*" * 50
     redirect_to '/'
   end
 
