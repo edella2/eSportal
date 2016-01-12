a47e5073b5bed81dbd52f5d3bb67f4e741819495
peterwiebe <pjwiebe@gmail.com>
2016-01-12 00:07:56 -0800

    Update event info to clean nbsp and include link to esportal page

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -7,30 +7,21 @@ class SubscriptionsController < ApplicationController
 
     @event = {
       'summary' => tournament.name,
-      'description' => tournament.description,
+      'description' => tournament.description.gsub("&nbsp;", " ") + " \n\n " + tournament_url(tournament),
       'location' => tournament.city,
       'start' => { 'dateTime' => DateTime.parse("#{tournament.start_date}") },
       'end' => { 'dateTime' => DateTime.parse("#{tournament.end_date}") } }
 
-    p @event
-    p "*"*50
-
+      p @event
+      p "*"*50
     client = Google::APIClient.new
-    p client
-    p "*"*50
     client.authorization.access_token = current_user.token
-    p client.authorization.access_token
-    p "*"*50
     service = client.discovered_api('calendar', 'v3')
-    p service
-    p "*"*50
 
     @set_event = client.execute(api_method: service.events.insert,
                                 parameters: { 'calendarId' => 'primary', 'sendNotifications' => true },
                                 body: JSON.dump(@event),
                                 headers: { 'Content-Type' => 'application/json' })
-    p @set_event
-    p "*"*50
     redirect_to '/'
   end
 
