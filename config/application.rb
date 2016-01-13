581328c968e908d87391840b2feb3c10be28dfc8
Steven Broderick <brodericksteven@gmail.com>
2016-01-12 16:40:18 -0800

    Reformat code

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module ESportal
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    binding.pry
   end
 end
