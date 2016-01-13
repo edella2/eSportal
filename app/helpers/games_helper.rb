fb9e2c6ac91dc0a8f59fd9898fa98fa6a8495a72
gastongouron <gastongouron@gmail.com>
2016-01-13 02:28:04 -0800

    Work views

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,7 @@
 module GamesHelper
+
+  def get_game_title(id)
+    Game.find(id).title
+  end
+
 end
--- db/migrate/20160108215932_create_games.rb
+++ db/migrate/20160108215932_create_games.rb
@@ -2,7 +2,6 @@ class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
       t.string :name
-
       t.timestamps null: false
     end
   end
